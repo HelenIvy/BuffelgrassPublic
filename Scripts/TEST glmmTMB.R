9d7d8cad415c0f537b787a65a6ed46e080f49c95
HelenIvy <102314768+HelenIvy@users.noreply.github.com>
2022-10-26 14:11:37 -0700

    cleaning file

--- Scripts/TEST glmmTMB.R
+++ Scripts/TEST glmmTMB.R
@@ -28,9 +28,10 @@ summary(DATAglmm)
 
 #repeated measures(1\SUBJECT) for the random subject effect 
 
-#try the glmmTMB, https://cran.r-project.org/web/packages/glmmTMB/vignettes/glmmTMB.pdf load packages
+#followed: https://cran.r-project.org/web/packages/glmmTMB/vignettes/glmmTMB.pdf
 
 install.packages("glmmTMB")
+citeNatbib("glmmTMB")
 install.packages("car")
 install.packages("MASS")
 install.packages("bbmle")
@@ -45,9 +46,7 @@ library("ggplot2")
 theme_set(theme_bw()+
             theme(panel.spacing=grid::unit(0,"lines")))
 
-#example set
 
-#JB SUGGESTS https://journal.r-project.org/archive/2017/RJ-2017-066/RJ-2017-066.pdf
 require(glmmTMB)
 require(car)
 fit_zipoisson <- glmmTMB(Individuals~TREATMENT + SEASONYEAR 
@@ -65,7 +64,6 @@ summary(fit_zinbinom1)
 fit_zinbinom2 <- update(fit_zipoisson,family=nbinom2)
 summary(fit_zinbinom2)
 
-#file:///Z:/Shared/Field%20Institute/Research/1%20Current%20Projects/Invasive%20Plants%20-%20Pennisetum%20removal%20experiments/Data/Analysis/BG%202022/Fall%202022%20estimates%20data%20and%20analysis/glmmTMB.pdf
 
 AICtab(fit_zipoisson,fit_zinbinom2,fit_zinbinom1)
 #best fit is zinbinom1
@@ -92,6 +90,7 @@ cld(marginal,
     alpha=0.05,
     Letters=letters,  ### Use lower-case letters for .group
     adjust="tukey")
+
 marginal = emmeans(fit_zinbinom1,
                    ~ SEASONYEAR)
 pairs(marginal,
