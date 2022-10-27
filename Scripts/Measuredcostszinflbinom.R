31d5a383d94ede6f185a9a56369dbe2230016aeb
HelenIvy <102314768+HelenIvy@users.noreply.github.com>
2022-10-27 13:59:45 -0700

    new zinfl script

---
+++ Scripts/Measuredcostszinflbinom.R
@@ -0,0 +1,90 @@
+
+#read in data
+library(readxl)
+
+
+BG_measured_costs_joined <- read_excel("Data/BG measured costs joined.xlsx", 
+                                       sheet = "noCNTorCR")
+View(BG_measured_costs_joined)
+
+
+DATACosts <- BG_measured_costs_joined
+
+# remove na in r - remove rows - na.omit function / option
+ompleterecords <- na.omit(DATACosts)
+
+#Set categorical factors for main effects:
+ DATACosts <- within( DATACosts, {
+
+
+  BLOCK<-factor(Block)
+  BLOCKPLOT<-factor(BlockPlot)
+  TREATMENT<-factor(Treatment)
+  YEAR<-factor(Year)
+  
+})
+#If use YEAR is categorical, if use Year is continous
+summary( DATACosts)
+
+
+#repeated measures(1\SUBJECT) for the random subject effect 
+
+
+
+
+#I ended up only including time and supplies and not hours in the MS because they were similar graphs;
+
+
+require(glmmTMB)
+require(car)
+fit_zipoisson <- glmmTMB(Timeandsupplies~TREATMENT*YEAR
+                         + (1|BLOCK)+(1|BLOCKPLOT),
+                         data=DATACosts,
+                         ziformula=~1,
+                         family=poisson)
+
+summary(fit_zipoisson)
+
+fit_zinbinom1b <- update(fit_zipoisson,family=nbinom1)
+summary(fit_zinbinom1b)
+
+
+fit_zinbinom2 <- update(fit_zipoisson,family=nbinom2)
+summary(fit_zinbinom2)
+
+
+AICtab(fit_zipoisson,fit_zinbinom2,fit_zinbinom1b)
+#best fit is fit_zipoisson the binomial models did not converge
+
+#using car anova https://cran.r-project.org/web/packages/glmmTMB/vignettes/model_evaluation.pdf
+library(car)
+
+Anova(fit_zipoisson,type="III")
+Anova(fit_zipoisson)
+
+#Post-hoc analysis can be conducted with the emmeans package.
+library(multcompView)
+require(multcompView)
+library(multcomp)
+require(multcomp)
+library(emmeans)
+require(emmeans)
+
+
+marginal = emmeans(fit_zipoisson,
+                   ~ TREATMENT)
+pairs(marginal,
+      adjust="tukey")
+cld(marginal,
+    alpha=0.05,
+    Letters=letters,  ### Use lower-case letters for .group
+    adjust="tukey")
+
+marginal = emmeans(fit_zipoisson,
+                   ~ YEAR)
+pairs(marginal,
+      adjust="tukey")
+cld(marginal,
+    alpha=0.05,
+    Letters=letters,  ### Use lower-case letters for .group
+    adjust="tukey")
